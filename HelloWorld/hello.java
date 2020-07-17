b04773f5fde11c50871783d5f9e2416e53287ba9
Jerry V <jvr572@gmail.com>
2020-07-17 14:38:03 -0400

    hello world

---
+++ HelloWorld/hello.java
@@ -0,0 +1,9 @@
+/**
+ * outputting hello world for the first time in java,
+ * learning the syntax and what everything means 
+ */
+public class hello {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
