c1d5258139b655317ea6657bde4cf08a95122249
Jerry V <jvr572@gmail.com>
2020-07-18 15:51:04 -0400

    create file for week 1 finishing up assignments

---
+++ CSPrincton/week1/CMYKtoRGB.java
@@ -0,0 +1,5 @@
+public class CMYKtoRGB {
+  public static void main(String[] args) {
+   
+  }
+}
---
+++ CSPrincton/week1/RightTriangle.java
@@ -0,0 +1,11 @@
+public class RightTriangle {
+
+  public static void main(String[] args) {
+    int a, b, c;
+    a = Integer.parseInt(args[0]);
+    b = Integer.parseInt(args[1]);
+    c = Integer.parseInt(args[2]);
+    boolean isRightLen = ( Math.pow(a,2)+Math.pow(b,2)) == Math.pow(c,2);
+    System.out.println(isRightLen);
+  }
+}
