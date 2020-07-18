a659aaf83e95892ddcb7a1cb15684984779ebaba
Jerry V <jvr572@gmail.com>
2020-07-18 17:48:04 -0400

    done week 1

--- CSPrincton/week1/CMYKtoRGB.java
+++ CSPrincton/week1/CMYKtoRGB.java
@@ -1,5 +1,18 @@
 public class CMYKtoRGB {
   public static void main(String[] args) {
-   
+    double c,m,y,k;
+    c = Double.parseDouble(args[0]);
+    m = Double.parseDouble(args[1]);
+    y = Double.parseDouble(args[2]);
+    k = Double.parseDouble(args[3]);
+    
+    double white = (1 - k);
+    int red = (int)Math.round((255 * white * (1-c)));
+    int blue = (int)Math.round((255 * white * (1-y)));
+    int green = (int)Math.round((255 * white * (1-m)));
+
+    System.out.println("red = " + red);
+    System.out.println("green = "+ green);
+    System.out.println("blue = " + blue);
   }
 }
--- CSPrincton/week1/GreatCircle.java
+++ CSPrincton/week1/GreatCircle.java
@@ -1,3 +1,26 @@
+import javax.crypto.CipherInputStream;
+
 public class GreatCircle {
+ public static void main(String[] args) {
+  double a,b,c,d;
+  a = Double.parseDouble(args[0]);
+  b = Double.parseDouble(args[1]);
+  c = Double.parseDouble(args[2]);
+  d = Double.parseDouble(args[3]);
+  // distance between latitudes and longitudes
+  double dLat = Math.toRadians(c - a);
+  double dLon = Math.toRadians(d - b);
+
+  // convert to radians
+  a = Math.toRadians(a);
+  c = Math.toRadians(c);
+
+  // apply formulae
+  double comp = Math.pow(Math.sin(dLat / 2), 2) + Math.pow(Math.sin(dLon / 2), 2) * Math.cos(a) * Math.cos(c);
+  double rad = 6371.0;
+  double ci = 2 * Math.asin(Math.sqrt(comp));
+  double path = rad * ci;
   
+  System.out.println(path + " kilometers");
+ } 
 }
